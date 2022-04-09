4395e965f8c917a4c747e4d5d91ff104568b87a9
Mazen Alotaibi <mazen.ota@gmail.com>
2022-04-09 05:07:58 -0700

    update tests_ci to be run on cpu

--- dev_install.sh
+++ dev_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-conda env create -f cpu_environment.yml
+conda env create -f dev_environment.yml
 # upgrading pip - https://stackoverflow.com/questions/61365790/error-could-not-build-wheels-for-scipy-which-use-pep-517-and-cannot-be-installe
 pip install pip --upgrade
 # disabling poetry's experimental new installer - https://github.com/python-poetry/poetry/issues/4210#issuecomment-877778420
