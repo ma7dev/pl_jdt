d8edbe1d6185ff42524eb833ea714f3b9f067eda
Mazen Alotaibi <mazen.ota@gmail.com>
2022-04-10 02:58:42 -0700

    run act locally and it passed! Testing on GitHub

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 conda env create -f environment.yml
+conda activate pl_jdt
 # upgrading pip - https://stackoverflow.com/questions/61365790/error-could-not-build-wheels-for-scipy-which-use-pep-517-and-cannot-be-installe
 pip install pip --upgrade
 # disabling poetry's experimental new installer - https://github.com/python-poetry/poetry/issues/4210#issuecomment-877778420
