ffc8cb337a6d615c296dc3a6a615dd38005d8d84
Mazen Alotaibi <mazen.ota@gmail.com>
2022-04-10 04:50:36 -0700

    add run_exp ci and remove unwanted files

--- tmp/tracking_wo_bnw/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='tracktor',
-      packages=find_packages(where='pl_jdt'),
-      package_dir={'':'pl_jdt'},
-      version='0.0.1',
-      install_requires=[],)
